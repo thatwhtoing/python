d967f3c5f61aab5260119fed944bd6fdc3a4004b
thatwhtoing <79578702+thatwhtoing@users.noreply.github.com>
2022-03-10 22:08:38 +0900

    Add files via upload

---
+++ 1. Algorithm Basics/Exercise 1-6.py
@@ -0,0 +1,14 @@
+# 실습 1-5의 원래 모습
+
+n = int(input('정수를 입력하세요.'))
+
+if n == 1:
+    print('A')
+elif n == 2:
+    print('B')
+elif n == 3:
+    print('C')
+else:
+    pass
+
+# else를 사용하지 않으면 else: pass가 실행된다고 보면 된다.
